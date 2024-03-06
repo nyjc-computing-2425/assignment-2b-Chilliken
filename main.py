977d40e0d511bfe39a058274a2f6a17e3e6b0014
Chilliken <74310036+Chilliken@users.noreply.github.com>
2024-03-06 04:24:30 +0000

    done

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 num = input('Enter a number (decimal only): ')
 # type your code here
-
+number = num
+number = number.lstrip("0123456789")
+number = number.replace(".","")
+dp = len(number)
 
 
 # do not change any code beyond this point
